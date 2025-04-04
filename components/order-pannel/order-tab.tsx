693d7dd076484104d62ccf80004143279c2b9072
huliang <huliang0222@gmail.com>
2025-04-04 20:13:59 +0800

    fix: price

--- components/order-pannel/order-tab.tsx
+++ components/order-pannel/order-tab.tsx
@@ -92,10 +92,13 @@ export function OrderTab({ poolId }: OrderTabProps) {
   const getCurrentPrice = (cpmmPoolInfo?: CpmmPoolInfo, isReverse = true): number => {
     if (!cpmmPoolInfo) return 0;
     const poolInfo = cpmmPoolInfo.poolInfo;
-    const amountA = new Decimal(poolInfo.mintAmountA).mul(10 ** poolInfo.mintA.decimals);
-    const amountB = new Decimal(poolInfo.mintAmountB).mul(10 ** poolInfo.mintB.decimals);
+    // const amountA = new Decimal(poolInfo.mintAmountA).mul(10 ** poolInfo.mintA.decimals);
+    // const amountB = new Decimal(poolInfo.mintAmountB).mul(10 ** poolInfo.mintB.decimals);
+    const amountA = new Decimal(poolInfo.mintAmountA);
+    const amountB = new Decimal(poolInfo.mintAmountB);
 
     const price = isReverse ? amountA.div(amountB).toNumber() : amountB.div(amountA).toNumber();
+    console.log("🚀 ~ getCurrentPrice ~ price:", price);
 
     return price;
   };
